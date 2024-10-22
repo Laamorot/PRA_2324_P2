3de31780b13b4a9f45e4e39163679f08fe5ac47b
LAAMOROT <laamorot@alumno.upv.es@ldsic-vdi08.upvnet.upv.es>
2024-10-22 20:29:39 +0200

    Changed testfile, further changes required

--- test_DyV.cpp
+++ test_DyV.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 #include <vector>
 #include "DyV.h"
+#include <chrono>
 
 using namespace std;
 
 int main() {
+
+auto start = std::chrono::system_clock::now();
+
 	std::vector<int> arr = {1,2,3,4,5,6,7,8,9,10};
 	int buscar = 5;
 	int resultado = BusquedaBinaria(buscar, arr, 0, arr.size()-1);
@@ -14,5 +18,22 @@ int main() {
 		cout << "Elemento " << buscar << " no se encuentra en el array." << std::endl;
 	}
 
-	return 0;
+auto end = std::chrono::system_clock::now();
+
+std::chrono::duration<float, std::milli> duration = end - start;
+
+std::cout << “Binarysearch time: “ << duration.count() << “s” << std::endl;
+
+
+auto start = std::chrono::system_clock::now();
+
+proceso_a_medir();
+
+auto end = std::chrono::system_clock::now();
+
+std::chrono::duration<float, std::milli> duration = end - start;
+
+std::cout << “Time: “ << duration.count() << “s” << std::endl;
+
+return 0;
 }
